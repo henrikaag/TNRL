b373f1a28a1e007913ed01ad5539f7daec4c4b64
henrikaag <aaghenrik@gmail.com>
2022-09-20 15:36:59 +0200

    1st commit

---
+++ src/components/Charts/LineChart.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const LineChart = () => {
+  return <div>$0</div>
+}
+
+export default LineChart
---
+++ src/pages/Ansvarlige.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Ansvarlige = () => {
+  return (
+    <div>Ansvarlige</div>
+  )
+}
+
+export default Ansvarlige
