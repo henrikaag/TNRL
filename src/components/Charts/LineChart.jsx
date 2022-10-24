fbdd71f2ea68a65d739ac81aab84aee03e80d0dd
henrikaag <aaghenrik@gmail.com>
2022-10-25 01:44:10 +0200

    fuckup

--- src/components/Charts/LineChart.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const LineChart = () => {
-  return <div>$0</div>
-}
-
-export default LineChart
--- src/pages/Ansvarlige.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Ansvarlige = () => {
-  return (
-    <div>Ansvarlige</div>
-  )
-}
-
-export default Ansvarlige
